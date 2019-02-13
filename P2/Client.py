ef9fcbba85065dc7b5b0cda656aa5d08eab5307c
gemabbaz <gemabb00@gmail.com>
2019-02-13 17:35:52 +0100

    This is practice 2

--- P2/Client.py
+++ P2/Client.py
@@ -1,24 +1,27 @@
 #We are gonna create the client program using the Seq class created in the previous practice
-
 import socket
 from Seq import Seq
-IP = "212.128.253.64"
-PORT = 8080
+#Here is the IP address of my computer
+IP = "212.128.253.106"
+PORT = 8080 #Chose the port 8080
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((IP, PORT))
 
 while True:
     chat = input("Introduce your message: ")
-    if chat == "Bye":
+    if chat == "Bye": #To close the program
         break
     s1 = Seq(chat)
-    s2 = s1.complement()
-    s3 = s1.reverse()
-    s.send(str.encode(s2))
-    s.send(str.encode(s3))
+    s2 = s1.complement() #Using complement function imported from Seq class
+    s3 = s1.reverse() #Using complement function imported from Seq class
+    text1 = "The complementary sequence is: "+s2.strbases
+    text2 = "\n The reverse sequence is: "+s3.strbases
+    s.send(str.encode(text1)) #Sending the output to the server
+    s.send(str.encode(text2))
+
 
     msg = s.recv(2048).decode("utf-8")
-    msg1 = s.recv(2048).decode("utf-8")
+
     print("MESSAGE FROM THE SERVER: \n")
     print(msg)
     s.close()
--- P2/Server.py
+++ P2/Server.py
@@ -2,7 +2,7 @@ import socket
 
 # Configure the Server's IP and PORT
 PORT = 8080
-IP = "192.168.1.36"
+IP = "212.128.253.106"
 MAX_OPEN_REQUESTS = 5
 
 # Counting the number of connections
@@ -31,7 +31,7 @@ try:
         msg = clientsocket.recv(2048).decode("utf-8")
         print("Message from client: {}".format(msg))
 
-        # Send the messag
+        # Send the message
         message = "Hello, welcome to this amazing program!! :)"
         send_bytes = str.encode(message)
         # We must write bytes, not a string
