be2f992c894b5c6ccb683f3fbafdc4f63540c3c6
gemabbaz <gemabb00@gmail.com>
2019-02-26 16:04:31 +0100

    Added configuration

---
+++ Session 11/Server.py
@@ -0,0 +1,74 @@
+import socket
+import termcolor
+
+# Change this IP to yours!!!!!
+IP = "212.128.253.96"
+PORT = 8086
+MAX_OPEN_REQUESTS = 5
+
+
+def process_client(cs):
+    """Process the client request.
+    Parameters:  cs: socket for communicating with the client"""
+
+    # Read client message. Decode it as a string
+    msg = cs.recv(2048).decode("utf-8")
+
+    # Print the received message, for debugging
+    print()
+    print("Request message: ")
+    termcolor.cprint(msg, 'green')
+
+    contents = """
+        <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+          <head>
+            <meta charset="utf-8">
+            <title>Green server</title>
+          </head>
+          <body style="background-color: lightpink;">
+            <h1>GREEN SERVER</h1>
+            <p>YOU ARE BEAUTIFUL! :-)</p>
+          </body>
+        </html>
+        """
+
+
+    status_line = "HTTP/1.1 200 ok\r\n" #We always have to finish this line with the \r\n
+    heather = "Content-Type: text/html\r\n" #Plain text when you are just sending a simple message, not an image, video etc.
+    heather += "Content-Lenth {}\r\n".format(len(str.encode(contents)))
+
+    response_msg = status_line + heather + "\n" + contents
+
+    cs.send(str.encode(response_msg))
+
+
+    # Close the socket
+    cs.close()
+
+
+# MAIN PROGRAM
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the IP and PORT
+serversocket.bind((IP, PORT))
+
+# Configure the server sockets
+# MAX_OPEN_REQUESTS connect requests before refusing outside connections
+serversocket.listen(MAX_OPEN_REQUESTS)
+
+print("Socket ready: {}".format(serversocket))
+
+while True:
+    # accept connections from outside
+    # The server is waiting for connections
+    print("Waiting for connections at {}, {} ".format(IP, PORT))
+    (clientsocket, address) = serversocket.accept()
+
+    # Connection received. A new socket is returned for communicating with the client
+    print("Attending connections from client: {}".format(address))
+
+    # Service the client
+    process_client(clientsocket)
