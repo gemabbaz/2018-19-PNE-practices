511a3dac32ddb9cf14788d40a419a452442d4589
gemabbaz <gemabb00@gmail.com>
2019-03-05 16:19:23 +0100

    Session 13

--- Session 13/WebServer1.py
+++ Session 13/WebServer1.py
@@ -2,12 +2,23 @@ import http.server
 import socketserver
 
 # Define the Server's port
-PORT = 8001
+PORT = 8002
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         print("GET received")
+        print("Request line" + self.requestline)
+        print("       Cmd:  " + self.command)
+        print("      Path:  " + self.path)
+
+        content = "I am the happy server! <3"
+        self.send_response(200);
+        self.send_header('Content-Type','text/plain')
+        self.send_header('Content-Length', len(str.encode(content)))
+        self.end_headers()
+
+        self.wfile.write(str.encode(content))
         return
 
 
