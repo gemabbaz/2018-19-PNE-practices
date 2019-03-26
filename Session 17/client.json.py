3f826f6bcf4014275592e1788ccb601ed03859b9
gemabbaz <gemabb00@gmail.com>
2019-03-26 17:15:13 +0100

    Session 17 with exercises 1, 2.

--- Session 17/client.json.py
+++ Session 17/client.json.py
@@ -4,7 +4,7 @@ import http.client
 import termcolor
 import json
 
-PORT = 8001
+PORT = 8004
 SERVER = 'localhost'
 
 
@@ -15,7 +15,7 @@ conn = http.client.HTTPConnection(SERVER, PORT)
 
 # -- Send the request message, using the GET method. We are
 # -- requesting the main page (/)
-conn.request("GET", "/")
+conn.request("GET", "/listusers")
 
 # -- Read the response message from the server
 r1 = conn.getresponse()
@@ -37,27 +37,18 @@ person = json.loads(data1)
 
 print("CONTENT: ")
 
-# Print the information in the object
-print()
-termcolor.cprint("Name: ", 'green', end="")
-print(person['Firstname'], person['Lastname'])
-
-termcolor.cprint("Age: ", 'green', end="")
-print(person['age'])
-
-# Get the phoneNumber list
-phoneNumbers = person['phoneNumber']
-
-# Print the number of elements int the list
-termcolor.cprint("Phone numbers: ", 'green', end='')
-print(len(phoneNumbers))
-
 # Print all the numbers
-for i, num in enumerate(phoneNumbers):
-    termcolor.cprint("  Phone {}:".format(i), 'blue')
+for element in person:
+    termcolor.cprint("Name: ", 'red', end="")
+    print(person['firstname'], person['lastname'])
+    termcolor.cprint("Age: ", 'red', end="")
+    print(person['age'])
+    termcolor.cprint("    Phones available: ", 'red', end='')
+    print(len(element["phone"]))
 
-    # The element num contains 2 fields: number and type
-    termcolor.cprint("    Type: ", 'red', end='')
-    print(num['type'])
-    termcolor.cprint("    Number: ", 'red', end='')
-    print(num['number'])
+    for i, num in enumerate(phone):
+        termcolor.cprint(   "Phone {} ".format(i), 'red', end='')
+        termcolor.cprint(   "Type: ", 'red', end='')
+        print(num["type"])
+        termcolor.cprint(   "Number: ", 'red', end='')
+        print(num["number"])
